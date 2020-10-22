2c7c374aff16975e44ba8bb99ab9742edb44684d
loading <49134864+load1n9@users.noreply.github.com>
2020-10-22 15:58:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ class MyClient(discord.Client):
 
             return
         spawn = random.randint(1,100)
+        print(spawn)
         try:
             fclient.query(
                 q.get(q.match(q.index("users_by_name"), str(message.author))))
